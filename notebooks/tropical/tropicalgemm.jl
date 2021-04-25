4294ba51af0fb7b557339aca31c57d61998ba20e
GiggleLiu <cacate0129@gmail.com>
2021-04-24 22:18:27 -0400

    update link to download

--- notebooks/tropical/tropicalgemm.jl
+++ notebooks/tropical/tropicalgemm.jl
@@ -13,6 +13,16 @@ md"# Speed up Tropical matrix multiplication"
 # ╔═╡ d238ceee-f8de-4e14-8947-636c5f879e8c
 md"By: GiggleLiu and Chris Elrod"
 
+# ╔═╡ 8fe28e17-e716-4a78-a0ba-d70712598a90
+html"""
+<div align="center">
+<a class="Header-link " href="https://github.com/TensorBFS/TropicalGEMM.jl" data-hotkey="g d" aria-label="Homepage " data-ga-click="Header, go to dashboard, icon:logo">
+  <svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+</a>
+<br>
+<a href="https://raw.githubusercontent.com/GiggleLiu/notebooks/master/notebooks/tropical/tropicalgemm.jl" target="_blank"> download this notebook </a></div>
+"""
+
 # ╔═╡ 56082ee0-898f-11eb-13fc-ab4eb456e479
 md"This blog is about how to make a GEMM extension for Tropical numbers ([TropicalGEMM.jl](https://github.com/TensorBFS/TropicalGEMM.jl/)), with a close to theoretical optimal performance. It is based on
 * [LoopVectorization.jl](https://github.com/JuliaSIMD/LoopVectorization.jl/), for vectorizing loops (i.e. utilizing SIMD),
@@ -399,6 +409,7 @@ However, composite types are not yet supported in `LoopVectorization`.
 end
 
 # ╔═╡ Cell order:
+# ╟─8fe28e17-e716-4a78-a0ba-d70712598a90
 # ╟─8d24b3aa-7853-11eb-0be4-23088fd5e70a
 # ╟─d238ceee-f8de-4e14-8947-636c5f879e8c
 # ╟─56082ee0-898f-11eb-13fc-ab4eb456e479
