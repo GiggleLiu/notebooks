3ffff8f2662f511f9f7363c4133a1c35ec2843b5
GiggleLiu <cacate0129@gmail.com>
2022-12-01 15:18:14 +0800

    update

--- livecoding/generate.sh
+++ livecoding/generate.sh
@@ -1,4 +1,13 @@
-#!/bin/env sh
-julia --project=$1 -e "using Pkg; Pkg.instantiate()"
-julia --project=$1 -e "using AsciinemaGenerator, InteractiveUtils; cast_file(\"$1/main.jl\"; output_file=\"$1/main.cast\", mod=Main, tada=true, height=30)"
-echo "DONE"
+#!/bin/bash
+DIRREL="$( dirname -- "${BASH_SOURCE[0]}"; )";   # Get the directory name
+BASEDIR="$( realpath -e -- "$DIRREL"; )";    # Resolve its full path if need be
+DIR="$( realpath -e -- "$BASEDIR"; )/$1";    # Resolve its full path if need be
+julia --project=$DIR -e "using Pkg; Pkg.instantiate()"
+julia --project=$DIR -e "using AsciinemaGenerator, InteractiveUtils; cast_file(\"$DIR/main.jl\"; output_file=\"$DIR/main.cast\", mod=Main, tada=true, height=30)"
+read -p "Run? [y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+    source $BASEDIR/play.sh $1
+then
+    exit 1
+fi
--- livecoding/play.sh
+++ livecoding/play.sh
@@ -1,5 +1,8 @@
 #!/bin/env sh
+DIRREL="$( dirname -- "${BASH_SOURCE[0]}"; )";   # Get the directory name
+BASEDIR="$( realpath -e -- "$DIRREL"; )";    # Resolve its full path if need be
 first_arg="$1"
+DIR="$( realpath -e -- "$BASEDIR"; )/$first_arg";    # Resolve its full path if need be
 shift
-asciinema play "$first_arg/main.cast" $@
+asciinema play "$DIR/main.cast" $@
 echo "DONE"
